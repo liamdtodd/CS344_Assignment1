b08a865bf3011565c3394dfa4228e0ed640d008e
Liam Todd <toddl@os1.engr.oregonstate.edu>
2022-10-06 13:44:34 -0700

    structs for moviebook, movie

---
+++ movie.h
@@ -0,0 +1,42 @@
+/*************************
+ * Liam Todd
+ * This file contains the struct for 'movie' and it's corresponding functions
+ * ***********************/
+
+#ifndef MOVIE_H
+#define MOIVE_H
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
+
+struct moviebook {
+	struct movie* data;
+};
+
+struct movie {
+	char* title;
+	int year;
+	char* language;	
+	double rating;
+};
+
+moviebook* create_moviebook(int num) {
+	moviebook* book = malloc(num * sizeof(moviebook));
+	int x = 0;
+	for (x = 0; x < num; x++);
+		book[x].data = create_movie();
+	return book;
+}
+
+movie* create_movie() {
+	movie* film = malloc(sizeof(movie));
+	
+	film.title = malloc(20 * sizeof(char));		//this needs to change to a var of bytes!
+	film.year = -1;
+	film.language = malloc(20 * sizeof(char));		//this needs to change to a var of bytes!
+	film.rating = -1;
+	
+	return film;
+}
