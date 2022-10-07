7f63b3235a8c34aa2d6e82ba26a80e7e359e20d2
Liam Todd <toddl@os1.engr.oregonstate.edu>
2022-10-07 13:10:48 -0700

    updated 10/7 @ 1:10

--- movie.h
+++ movie.h
@@ -5,53 +5,16 @@
 
 #ifndef MOVIE_H
 #define MOIVE_H
-#include <iostream>
+//#include <iostream>
 #include <string.h>
 #include <stdlib.h>
 
-using namespace std;
+//using namespace std;
 
-/*
-struct moviebook {
-	struct movie* data;
-};
-*/
+struct movie;
 
-struct movie {
-	char* title;
-	int year;
-	char* language;	
-	double rating;
-};
+struct movie* create_movie();
+void free_movie(struct movie* film);
+//struct movie& operator=(const struct movie&);
 
-movie* create_movie();
-void free_movie(movie* film);
-/*
-moviebook* create_moviebook(int num) {
-	moviebook* book = malloc(num * sizeof(moviebook));
-	int x = 0;
-	for (x = 0; x < num; x++);
-		book[x].data = create_movie();
-	return book;
-}
-
-
-movie* create_movie() {
-	movie* film = malloc(sizeof(movie));
-	
-	film.title = malloc(20 * sizeof(char));		//this needs to change to a var of bytes!
-	film.year = -1;
-	film.language = malloc(20 * sizeof(char));		//this needs to change to a var of bytes!
-	film.rating = -1;
-	
-	return film;
-}
-
-void free_movie(movie* video) {
-	free(video.film);
-	free(video.language);
-	video.film = NULL;
-	video.language = NULL;
-}
-*/	
 #endif
