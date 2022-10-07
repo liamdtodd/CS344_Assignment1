7fc1776ede916be9eb1ce6d123262b3a764eb648
Liam Todd <toddl@os1.engr.oregonstate.edu>
2022-10-07 13:11:09 -0700

    updated 10/7 @ 1:10

--- movie.c
+++ movie.c
@@ -3,25 +3,43 @@
  * the .c implementation of movie.h
  * *****************/
 #include "movie.h"
-#include <iostream>
+//#include <iostream>
 #include <stdlib.h>
+#include <string.h>
 
-using namespace std;
+//using namespace std;
 
-movie* create_movie() {
-	movie* film = malloc(sizeof(movie));
+struct movie {
+	char* title;
+	char* language;
+	int year;
+	double rating;
+};
+
+struct movie* create_movie() {
+	struct movie* film = malloc(sizeof(struct movie));
 	
-	film.title = malloc(20 * sizeof(char));			//this needs to be changed to a var amount of bytes!
-	film.year = -1;
-	film.language = malloc(20 * sizeof(char));		// this needs to be changed to a var amount of bytes!
-	film.rating = -1;
+	film->title = malloc(20 * sizeof(char));			//this needs to be changed to a var amount of bytes!
+	film->year = -1;
+	film->language = malloc(20 * sizeof(char));		// this needs to be changed to a var amount of bytes!
+	film->rating = -1;
 	
 	return film;
 }
 
-void free_movie(movie* film) {
-	free(film.title);
-	free(film.language);
-	film.title = NULL;
-	film.language = NULL;
-}	
+void free_movie(struct movie* film) {
+	free(film->title);
+	free(film->language);
+	film->title = NULL;
+	film->language = NULL;
+}
+
+/*
+struct movie& operator=(const struct movie& obj) {
+	strcpy(title, obj->title);
+	year = obj->year;
+	strcpy(language, obj->language);	
+	rating = obj->rating;
+	return *this;
+}
+*/
