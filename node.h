15f6e738f2647efd1445fdd7a344ae23f8843935
Liam Todd <toddl@os1.engr.oregonstate.edu>
2022-10-07 19:27:54 -0700

    bug fixes

--- node.h
+++ node.h
@@ -4,16 +4,11 @@
  * ***************/
 #ifndef NODE_H
 #define NODE_H
-//#include <iostream>
 #include "movie.h"
-
-//using namespace std;
 
 struct node {
 	struct movie* video;
 	struct node* next;
 };
-
-//struct node* create_node();
 
 #endif
