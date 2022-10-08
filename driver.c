d2115f9f65d8912108b5cbce5ecdb8167b7367b3
Liam Todd <toddl@os1.engr.oregonstate.edu>
2022-10-07 19:28:44 -0700

    added opening the file for reading

--- driver.c
+++ driver.c
@@ -9,15 +9,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+void readData(struct Linked_List* list, char* filepath) {
+	FILE* movieData = fopen(filepath, "r");
+	
+	if (movieData == NULL) {
+		fprintf(stderr, "Could not open file: %s\n", filepath);
+		exit(1);
+	}
+
+	fclose(movieData);
+}
+
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
 		printf("Not enough arguments entered! Example execution: 'a.out example.csv'\n");
 		return 0;
-	}
+	}							//ensuring that the program is started with a file name
+
+	char* filepath = argv[1];
 	struct Linked_List* list = create_linkedlist();
 
-	free_listelements(list);
+	readData(list, filepath);
+	printf("Successfully read from %s and parsed through %d movies\n", filepath, list->length);
 
+	free_listelements(list);
 	free(list);
 
 	return 0;
