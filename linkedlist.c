a95f00e84569dd320eb46e1b3fd8b216ac4d9ace
Liam Todd <toddl@os1.engr.oregonstate.edu>
2022-10-07 19:28:37 -0700

    bug fixes

--- linkedlist.c
+++ linkedlist.c
@@ -5,17 +5,17 @@
 #include "node.h"
 #include "movie.h"
 #include "linkedlist.h"
-//#include <iostream>
 #include <stdlib.h>
 
-//using namespace std;
-
+/*
 struct Linked_List {
 	int length;
 	struct node* head;
 	struct node* tail;
 };
+*/
 
+//this function dynamically creates the linked list
 struct Linked_List* create_linkedlist() {
 	struct Linked_List* list = malloc(sizeof(struct Linked_List));
 	list->length = 0;
@@ -25,12 +25,14 @@ struct Linked_List* create_linkedlist() {
 	return list;
 }
 
+//this function checks to see if the list is empty
 int check_empty(struct node* head_temp) {
 	if (head_temp == NULL)
-		return 1;
-	return 0;
+		return 1;	//1 is true
+	return 0;		//0 is false
 }
 
+//this function is specifically for inserting the first element to the linked list
 void insert_firstelement(struct Linked_List* list, struct movie* film) {
 	struct node* n1 = malloc(sizeof(struct node));
 	n1->video = film;
@@ -39,9 +41,10 @@ void insert_firstelement(struct Linked_List* list, struct movie* film) {
 	list->tail = n1;
 }
 
+//this function adds to the front of the linked list, at the head
 void add_front(struct Linked_List* list, struct movie* film) {
 	if (check_empty(list->head) == 1)
-		insert_firstelement(list, film);
+		insert_firstelement(list, film);		//checking to see if this will be the first element inserted
 	else {
 		struct node* n1 = malloc(sizeof(struct node));
 		n1->video = film;
@@ -53,9 +56,10 @@ void add_front(struct Linked_List* list, struct movie* film) {
 	list->length++;
 }
 
+//this function adds to the back of the linked list, at the tail
 void add_back(struct Linked_List* list, struct movie* film) {
 	if (check_empty(list->head) == 1)
-		insert_firstelement(list, film);
+		insert_firstelement(list, film);		//checking to see if this will be the first element inserted
 	else {
 		struct node* n1 = malloc(sizeof(struct node));
 		n1->video = film;
@@ -66,13 +70,14 @@ void add_back(struct Linked_List* list, struct movie* film) {
 	list->length++;
 }
 
+//this function frees all the dynamically allocated data associated with the linked list
 void free_listelements(struct Linked_List* list) {
 	int x = 0;
 	for (x; x < list->length; x++) {
 		struct node* temp;
 		temp = list->head;
 		list->head = list->head->next;
-		free_movie(temp->video);
-		free(temp);
+		free_movie(temp->video);		//freeing the data within the node
+		free(temp);				//freeing the data in the linked list
 	}
 }	
